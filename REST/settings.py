d73e71ec9db0b351ef1fa2c2aff06f105998194c
nealwaga <waganeal@gmail.com>
2023-02-22 18:53:24 +0300

    added libraries to installed apps

--- REST/settings.py
+++ REST/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'accounts',
+    'rest_framework',
+    'knox',
 ]
 
 MIDDLEWARE = [
