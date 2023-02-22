8c5f1d3505b8970d926c3298dd433a1deb0b2dcd
nealwaga <waganeal@gmail.com>
2023-02-22 18:57:56 +0300

    made knox’s TokenAuthentication the default authentification class

--- REST/settings.py
+++ REST/settings.py
@@ -125,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# allows both session based and token based authentication
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'knox.auth.TokenAuthentication',
+    ]
+}
